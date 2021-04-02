42e79dd0fe59645474115b7b38f4e134b19f6932
dcarrera10 <denice.carrera10@gmail.com>
2021-04-01 21:00:19 -0500

    Updated Scripts
    the scripts and dropdown and slider were updated

--- GroupAssignment1/Assets/Scripts/CameraManagerScript.cs
+++ GroupAssignment1/Assets/Scripts/CameraManagerScript.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Camera mainCamera;
+    public Camera picInPic;
+    public Camera sideViewCamera;
+    public Camera splitUpper;
+    public Camera splitLower;
+
     void Start()
     {
         
@@ -13,6 +20,19 @@ public class CameraManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void OnSliderValueChanged(float value)
+    {
+        Debug.LogFormat("Slider:{0}", value);
+    }
+
+    public void SwitchViews (Dropdown change)
+    {
+        Debug.LogFormat("{0}:{1}", change.itemText.text,
+            change.value);
+    }
+
+
 }
